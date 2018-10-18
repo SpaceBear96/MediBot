54a715b4cce0378ed8d732e20802469e12133c2a
jfabiant <rronaldo_jhon_20@hotmail.com>
2018-10-18 05:30:48 -0500

    WatsonAPI actualizado

--- WatsonAPI/principal.py
+++ WatsonAPI/principal.py
@@ -6,21 +6,26 @@ import json
 conversation = watson_developer_cloud.ConversationV1(
     username='d66aabf9-fd1d-460c-b827-4d5dfbfa87d6',
     password='M32F5Ocn4GFf',
-    version='2018-09-20'
+    version='2018-07-10'
 )
 
-txtMsj = input("Escribir un mensaje: ")
+txtMsj = ""
 
-##context = {}
+while txtMsj != "bye":
+    
+    txtMsj = input(">> ")
 
-response = conversation.message(
-    workspace_id='834e8f0f-b69f-445c-a124-6323e38c6ad9',
-    input={
-        'text': txtMsj
-    }
-).get_result()
+    response = conversation.message(
+        workspace_id='834e8f0f-b69f-445c-a124-6323e38c6ad9',
+        input={
+            'text': txtMsj
+        }
+    ).get_result()
 
-print(json.dumps(response, indent=2))
+    ##response -> object
+    print(json.dumps(response['output']['text'], indent=2))
+    
+    
 
 
 
